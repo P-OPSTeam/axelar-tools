01a51180ceb3ebae0d38a3d10d8936023a0b5e65
pops <contact@pops.one>
2021-08-28 18:06:40 +0000

    [grafana] add notification channel provisionning

--- monitoring/start.sh
+++ monitoring/start.sh
@@ -1,3 +1,9 @@
+# Configure your notifier channel :
+ALERT_TELEGRAM_BOT_TOKEN="putyourbottokenhere"
+ALERT_TELEGRAM_CHAT_ID="putyourchatidhere"
+sed -i "s/TGBOTTOKEN/${ALERT_TELEGRAM_BOT_TOKEN}/g" data/notifiers/notify.yml
+sed -i "s/TGCHATID/${ALERT_TELEGRAM_CHAT_ID}/g" data/notifiers/notify.yml
+
 ADMIN_USER="admin" \
 ADMIN_PASSWORD="putyourpassword" \
 GF_USERS_ALLOW_SIGN_UP=false \
