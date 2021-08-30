5d354beb98d14f4f05693e18d9a7f98c9237ce07
rooiie <55528739+rooiie@users.noreply.github.com>
2021-08-30 09:13:40 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,9 +8,12 @@ else
     reset="false"
 fi 
 
+#kill tmux session
+tmux kill-session -t remote
+
 # stop axelar core
-sudo docker stop tofnd axelar-core
-sudo docker rm tofnd axelar-core
+sudo docker stop tofnd axelar-core 2> /dev/null
+sudo docker rm tofnd axelar-core 2> /dev/null
 
 # Determining Axelar versions
 TOFND_VERSION=$(curl -s https://raw.githubusercontent.com/axelarnetwork/axelarate-community/main/documentation/docs/testnet-releases.md | grep tofnd | cut -d \` -f 4)
@@ -18,20 +21,26 @@ CORE_VERSION=$(curl -s https://raw.githubusercontent.com/axelarnetwork/axelarate
 
 # echo ${TOFND_VERSION} ${CORE_VERSION}
 
-# Clone Axerlar Community Github
 
+echo "Clone Axerlar Community Github"
 # Remove repo for a clean git clone
 sudo rm -rf ~/axelarate-community/
 
 cd ~
-git clone https://github.com/axelarnetwork/axelarate-community.git
+git clone https://github.com/axelarnetwork/axelarate-community.git  >/dev/null 2>&1
 cd ~/axelarate-community
 
-# start the validator
-if [[ "$reset" =~ "false" ]]; then
-    tmux new -s "remote" -d
-    tmux send-keys -t "remote" "sudo join/joinTestnet.sh --axelar-core ${CORE_VERSION} --tofnd ${TOFND_VERSION} &>> testnet.log" C-m 
+echo "start the validator"
+
+# test if the axelarate_default docker network is created
+docker network ls | grep axelarate_default > /dev/null
+if [[ $? -eq 1 ]]; then
+    docker network create axelarate_default
+fi
+
+tmux new -s "remote" -d 2> /dev/null
+if [[ "$reset" =~ "false" ]]; then    
+    tmux send-keys -t "remote" "sudo join/joinTestnet.sh --axelar-core ${CORE_VERSION} &>> testnet.log" C-m 
 else
-    tmux new -s "remote" -d
-    tmux send-keys -t "remote" "sudo join/joinTestnet.sh --axelar-core ${CORE_VERSION} --tofnd ${TOFND_VERSION} --reset-chain  &>> testnet.log" C-m
+    tmux send-keys -t "remote" "sudo join/joinTestnet.sh --axelar-core ${CORE_VERSION} --reset-chain  &>> testnet.log" C-m
 fi
