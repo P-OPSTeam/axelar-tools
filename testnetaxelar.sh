c91bdbb6a1164fb0cc610bd7f5e80c26fe357b06
pops <contact@pops.one>
2021-09-11 17:31:02 +0000

    testnetaxelar.sh - improve flow of execution

--- testnetaxelar.sh
+++ testnetaxelar.sh
@@ -1,45 +1,71 @@
+#! /bin/bash
+
 # update repository's
-echo "updating ubuntu Repository's"
-sudo apt-get update 2> /dev/null
+echo "Updating ubuntu Repository's"
+sudo apt-get update > /dev/null 2>&1
+echo "done" 
+echo
 
 # upgrade node
-echo upgrading ubuntu
-sudo apt-get upgrade -y 2> /dev/null
+echo "Upgrading ubuntu"
+sudo apt-get upgrade -y > /dev/null 2>&1
+echo "done" 
+echo
 
 # install docker dependencies
-echo installing dependencies for docker
+echo "Installing dependencies for docker"
 sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release 2> /dev/null
+    lsb-release > /dev/null 2>&1
+echo "done"
+echo 
 
+echo "Setup docker repo"
 # Curl docker GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+if [[ ! -e "/usr/share/keyrings/docker-archive-keyring.gpg" ]]; then
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg > /dev/null 2>&1
+fi
 
 # setup docker repo
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null 2>&1
 
 # update repository's
-sudo apt-get update
+sudo apt-get update > /dev/null 2>&1
 
-echo installing docker
+echo "done"
+echo 
+
+echo "Installing docker"
 # Install docker Engine
 
-sudo apt-get install docker-compose docker-ce docker-ce-cli containerd.io -y 2> /dev/null
+echo "--> Install"
+sudo apt-get install docker-compose docker-ce docker-ce-cli containerd.io -y > /dev/null 2>&1
 
-# add user to the docker group
-sudo usermod -aG docker $USER
+echo "--> Add current user to docker group if necessary"
+id | grep docker > /dev/null 2>&1
+if [[ $? -eq 1 ]]; then
+  # add user to the docker group
+  sudo usermod -aG docker $USER 
+  echo "Please exit the terminal, log back in and restart the installation"
+  #activate changes to the group docker (linux only)
+  exit
+fi
 
+echo "--> Make sure services are started"
 # make sure docker services are started
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
+sudo systemctl enable docker.service > /dev/null 2>&1
+sudo systemctl enable containerd.service > /dev/null 2>&1
+
+echo "done"
+echo 
 
 # install jq
-sudo apt-get install jq -y 2> /dev/null
+sudo apt-get install jq -y > /dev/null 2>&1
 
 # run the validator
 bash run.sh reset
