b2874f18fc01c78920d5d068cd40bdc7fff3f376
rooiie <55528739+rooiie@users.noreply.github.com>
2021-09-13 17:00:58 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>run.log 2>&1
 # Everything below will go to the file 'run.log':
-echo "logs can be found in ~/axelar-tools/run.log
+echo "logs can be found in ~/axelar-tools/run.log"
 
 echo "Determining script path" >&3
 SCRIPT=`realpath -s $0`
