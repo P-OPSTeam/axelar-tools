8a56bac240ed96e9e46b9049920ccd5412e70fc4
rooiie <55528739+rooiie@users.noreply.github.com>
2021-10-08 18:23:48 +0200

    Update newvalidator.sh

--- newvalidator.sh
+++ newvalidator.sh
@@ -1,46 +1,57 @@
-echo "Determining script path" >&3
+echo "Determining script path" 
 SCRIPT=`realpath -s $0`
 SCRIPTPATH=`dirname $SCRIPT`
-echo "done" >&3
-echo >&3
+echo "done"
+echo
 
 sudo docker stop axelar-core
 
-sudo rm ~/.axelar_testnet/shared/config.toml
+echo "Modifying config.toml for using own Ropsten and tbtc node"
 
-sudo cp ~/axelarate-community/join/config.toml ~/.axelar_testnet/shared/
+sudo sed -i '/^# Address of the bitcoin RPC server/{n;d}' /root/axelar_testnet/shared/config.toml
 
-sudo sed -i '/^# Address of the bitcoin RPC server/{n;d}' ~/.axelar_testnet/shared/config.toml
-
-sudo sed -i '/^# Address of the ethereum RPC proxy/{n;d}' ~/.axelar_testnet/shared/config.toml
+sudo sed -i '/^# Address of the ethereum RPC proxy/{n;d}' /root/.axelar_testnet/shared/config.toml
 
 echo "Type in your btc node address with double quotes!"
 
 read btc
 
-sudo sed -i "/^# Address of the bitcoin RPC server/a rpc_addr    = "$btc"" ~/.axelar_testnet/shared/config.toml
+sudo sed -i "/^# Address of the bitcoin RPC server/a rpc_addr    = "$btc"" /root/.axelar_testnet/shared/config.toml
 
 echo "Type in your ETH Ropsten node address with double quotes!"
 
 read ETH
 
-sudo sed -i "/^# Address of the ethereum RPC proxy/a rpc_addr    = "$ETH"" ~/.axelar_testnet/shared/config.toml
+sudo sed -i "/^# Address of the ethereum RPC proxy/a rpc_addr    = "$ETH"" /root/.axelar_testnet/shared/config.toml
+
+echo "done"
+
+echo
 
 bash $SCRIPT/run.sh
+
+
+echo "Setting up validator config"
 
 echo "Name for your validator"
 
 read validator
 
-echo "amount of selfstake axltest example: 90000000axltest"
+echo "amount of selfstake axltest example: 90000000uaxl"
 
-read axltest
+read uaxl
 
 axelarvaloper=$(sudo docker exec axelar-core axelard tendermint show-validator)
 
 #from=$(sudo docker exec axelar-core axelard keys show validator -a)
 
-sudo docker exec -it axelar-core axelard tx staking create-validator --yes --amount "$axltest" --moniker "$validator" --commission-rate="0.10" --commission-max-rate="0.20" --commission-max-change-rate="0.01" --min-self-delegation="1" --pubkey $axelarvaloper --from validator -b block
+echo "done"
+
+echo
+
+echo "Starting the validator"
+
+sudo docker exec -it axelar-core axelard tx staking create-validator --yes --amount "$uaxl" --moniker "$validator" --commission-rate="0.10" --commission-max-rate="0.20" --commission-max-change-rate="0.01" --min-self-delegation="1" --pubkey $axelarvaloper --from validator -b block
 
 sleep 10
 
@@ -50,6 +61,12 @@ sudo docker exec -it axelar-core axelard q staking validator "$validator" | grep
 
 sudo docker exec -it axelar-core axelard tx snapshot registerProxy broadcaster --from validator -y
 
+echo "done"
+
+echo
+
+echo "Starting prereq docker containers"
+
 CORE_VERSION=$(curl -s https://raw.githubusercontent.com/axelarnetwork/axelarate-community/main/documentation/docs/testnet-releases.md  | grep axelar-core | cut -d \` -f 4)
 echo ${CORE_VERSION}
 
@@ -57,6 +74,12 @@ echo ${CORE_VERSION}
 TOFND_VERSION=$(curl -s https://raw.githubusercontent.com/axelarnetwork/axelarate-community/main/documentation/docs/testnet-releases.md  | grep tofnd | cut -d \` -f 4)
 echo ${TOFND_VERSION}
 
-bash ~/axelarate-community/join/launchValidator.sh --axelar-core $CORE_VERSION --tofnd $TOFND_VERSION 
+cd ~/axelarate-community
 
+bash join/launchValidator.sh --axelar-core $CORE_VERSION --tofnd $TOFND_VERSION 
 
+echo "done"
+
+echo
+
+echo "validator is setup, ask for extra uaxl from team members"
