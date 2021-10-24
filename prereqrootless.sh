712e363e744d4cb78a047b4e8919b5bf636222bc
rooiie <55528739+rooiie@users.noreply.github.com>
2021-10-24 13:42:37 +0200

    Create prereqrootless.sh

---
+++ prereqrootless.sh
@@ -0,0 +1,81 @@
+#! /bin/bash
+
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>testnetaxelar.log 2>&1
+# Everything below will go to the file 'testnetaxelar.log':
+echo "logs can be found in testnetaxelar.log"
+
+echo "Determining script path" >&3
+SCRIPT=`realpath -s $0`
+SCRIPTPATH=`dirname $SCRIPT`
+echo "done" >&3
+echo >&3
+
+# update repository's
+echo "Updating ubuntu repository's" >&3
+sudo apt-get update
+echo "done" >&3
+echo >&3
+
+# upgrade node
+echo "Upgrading ubuntu" >&3
+sudo apt-get upgrade -y
+echo "done" >&3
+echo >&3
+
+###    packages required: jq, bc
+REQUIRED_PKG="bc"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+    echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+    sudo apt-get --yes install $REQUIRED_PKG
+fi
+
+REQUIRED_PKG="jq"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+    echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+    sudo apt-get --yes install $REQUIRED_PKG
+fi
+
+# install docker dependencies
+echo "Installing dependencies for docker" >&3
+sudo apt install uidmap
+echo "done" >&3
+echo >&3
+
+# update repository's
+sudo apt-get update
+
+echo "done" >&3
+echo >&3
+
+echo "Installing docker" >&3
+# Install docker Engine
+
+echo "--> Install" >&3
+curl -fsSL https://get.docker.com/rootless | sh
+
+echo "--> Make sure services are started" >&3
+# make sure docker services are started
+userid=$(id -u)
+systemctl --user start docker 
+systemctl --user enable docker
+sudo loginctl enable-linger $(whoami)
+export export PATH=/home/$(whoami)/bin:$PATH
+export DOCKER_HOST=unix:///run/user/$userid/docker.sock
+
+echo "done" >&3
+echo >&3
+
+exec 2>&4 1>&3
+ 
+echo
+echo "Prereq done, start option 2 in the menu"
+echo
+read -n 1 -s -r -p "Press any key to go back to the menu" 
+
+bash $SCRIPTPATH/AxelarMenu.sh
