96439cb9b96219f60893fe119707d3feaee5251a
rooiie <55528739+rooiie@users.noreply.github.com>
2022-01-08 10:49:39 +0100

    Update nodemonitor.sh

--- monitoring/nodemonitor.sh
+++ monitoring/nodemonitor.sh
@@ -26,6 +26,7 @@ CONFIG=""                # config.toml file for node, eg. $HOME/.gaia/config/con
 ### optional:            #
 NPRECOMMITS="20"         # check last n precommits, can be 0 for no checking
 VALIDATORADDRESS=""      # if left empty default is from status call (validator)
+AXELAR_PASSWORD=""       # if left empty monitoring won't work
 AXELARVALIDATORADDRESS="" #if left empty default is from status call (axelar validator)
 CHECKPERSISTENTPEERS="1" # if 1 the number of disconnected persistent peers is checked (when persistent peers are configured in config.toml)
 VALIDATORMETRICS="on"    # metrics for validator node
@@ -394,6 +395,11 @@ if [ -z $CONFIG ]; then
     fi
 fi
 
+if [ -z $KEYRING_PASSWORD]; then
+    echo "Please enter the password one time below, if setting up as a service fill the field in the script"
+    read "Enter your password for polling the keys :" KEYRING_PASSWORD
+fi
+
 if [ -z $CONFIG ]; then
     echo "please configure config.toml in script"
     exit 1
@@ -428,7 +434,7 @@ then
     if [[ -f ~/.axelar_testnet/bin/axelard ]]; then 
         AXELARVALIDATORADDRESS=$(~/.axelar_testnet/bin/axelard keys show validator --bech val -a --home ~/.axelar_testnet/.core);
     else 
-        AXELARVALIDATORADDRESS=$(docker exec axelar-core axelard keys show validator --bech val -a);
+        AXELARVALIDATORADDRESS=$(docker exec axelar-core sh -c "echo $KEYRING_PASSWORD | axelard keys show validator --bech val -a");
     fi
 fi
 if [ -z $AXELARVALIDATORADDRESS ]; then
@@ -657,7 +663,7 @@ while true ; do
             fi
 
             echo -n "health-check is : "
-            docker exec vald axelard health-check --tofnd-host tofnd --operator-addr $(cat ~/.axelar_testnet/shared/validator.bech) --node http://axelar-core:26657 > /home/$USER/axelar-tools/monitoring/healthcheck.log
+            docker exec vald sh -c "echo $KEYRING_PASSWORD | axelard health-check --tofnd-host tofnd --operator-addr $(cat ~/.axelar_testnet/shared/validator.bech) --node http://axelar-core:26657" > /home/$USER/axelar-tools/monitoring/healthcheck.log
             if grep -q -F "failed" "/home/$USER/axelar-tools/monitoring/healthcheck.log"; then
                 echo 'not ok'
                 Health_check_status="NOK"
