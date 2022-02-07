32fc36be97da20bd10ab9e0023f443a1b8372f17
rooiie <55528739+rooiie@users.noreply.github.com>
2022-02-07 12:40:30 +0100

    Update Upgradevalidator.sh

--- Upgradevalidator.sh
+++ Upgradevalidator.sh
@@ -25,14 +25,14 @@ if [ $NETWORK == testnet ]; then
     echo "Network switched to Testnet"
     NETWORKPATH=".axelar_testnet"
     CONFIG=$HOME/$NETWORKPATH/.core/config/config.toml
-    CORE_VERSION=$(curl -s https://raw.githubusercontent.com/axelarnetwork/axelarate-community/main/resources/testnet-releases.md  | grep axelar-core | cut -d \` -f 4 | cut -d \v -f2)
-    TOFND_VERSION=$(curl -s https://raw.githubusercontent.com/axelarnetwork/axelarate-community/main/resources/testnet-releases.md  | grep tofnd | cut -d \` -f 4 | cut -d \v -f2)
+    CORE_VERSION=$(curl -s https://raw.githubusercontent.com/axelarnetwork/axelarate-community/main/resources/testnet-releases.md  | grep axelar-core | cut -d \` -f 4)
+    TOFND_VERSION=$(curl -s https://raw.githubusercontent.com/axelarnetwork/axelarate-community/main/resources/testnet-releases.md  | grep tofnd | cut -d \` -f 4)
     else
     echo "Network switched to Mainnet"
     NETWORKPATH=".axelar"
     CONFIG=$HOME/$NETWORKPATH/.core/config/config.toml
-    CORE_VERSION=$(cat ~/validators/resources/mainnet-releases.md | grep axelar-core | cut -d \` -f 4 | cut -d \v -f2)
-    TOFND_VERSION=$(cat ~/validators/resources/mainnet-releases.md | grep tofnd | cut -d \` -f 4 | cut -d \v -f2)
+    CORE_VERSION=$(cat ~/validators/resources/mainnet-releases.md | grep axelar-core | cut -d \` -f 4)
+    TOFND_VERSION=$(cat ~/validators/resources/mainnet-releases.md | grep tofnd | cut -d \` -f 4)
 fi
 
 if [ -z $CONFIG ]; then 
