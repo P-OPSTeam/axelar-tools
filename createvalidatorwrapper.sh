4ab9850020bfca69b49b2e5589632b3ce7c4d7c4
rooiie <55528739+rooiie@users.noreply.github.com>
2022-02-18 18:34:18 +0100

    Update createvalidatorwrapper.sh

--- createvalidatorwrapper.sh
+++ createvalidatorwrapper.sh
@@ -5,6 +5,43 @@ done
 
 if [[ "$createvalidator" == "yes" ]]; then
 
+denom=uaxl
+
+if [ -z $MONIKER ]; then
+    echo "Please enter Moniker name below"
+    read -p "Enter Moniker name :" MONIKER
+fi
+
+if  [ -z "$NETWORK" ];then
+    read -p "Enter network, testnet or mainnet :" NETWORK
+fi
+
+read -p "Enter your KEYRING PASSWORD : " KEYRING
+
+# Determining Axelar versions
+echo "Determining Axelar version"
+CORE_VERSION=$(curl -s https://docs.axelar.dev/resources/$NETWORK-releases.md | grep axelar-core | cut -d \` -f 4)
+echo ${CORE_VERSION}
+
+echo "Determining Tofnd version"
+TOFND_VERSION=$(curl -s https://docs.axelar.dev/resources/$NETWORK-releases.md  | grep tofnd | cut -d \` -f 4)
+echo ${TOFND_VERSION}
+echo
+
+if [ "$NETWORK" == testnet ]; then
+echo "Setup node for axelar testnet"
+echo "Determining testnet chain"
+CHAIN_ID=$(curl -s https://raw.githubusercontent.com/axelarnetwork/axelarate-community/main/scripts/node.sh | grep chain_id=axelar-t | cut -f2 -d "=")
+NETWORKPATH=".axelar_testnet"
+else
+echo "Setup node for axelar mainnet"
+echo "Determining mainnet chain"
+CHAIN_ID=$(curl -s https://raw.githubusercontent.com/axelarnetwork/axelarate-community/main/scripts/node.sh | grep chain_id=axelar-d | cut -f2 -d "=")
+NETWORKPATH=".axelar"
+fi
+
+cd $HOME/axelarate-community/
+
 echo "install validator tools"
 KEYRING_PASSWORD=$KEYRING TOFND_PASSWORD=$KEYRING ./scripts/validator-tools-host.sh n $NETWORK
 
@@ -16,10 +53,12 @@ echo
 broadcaster=$(tail $HOME/$NETWORKPATH/broadcaster.txt | grep address | cut -f2 -d ":")
 echo "broadcaster adress is : $broadcaster"
 
-read -rsn1 -p"If funded the addresses press any key to continue";echo
+read -rsn1 -p"Please copy and fund the address, do not use ctrl-c";echo
+read -rsn1 -p"If funded press enter";echo
 
 echo "register proxy"
 broadcaster=$(tail $HOME/$NETWORKPATH/broadcaster.address)
+validator=$(tail $HOME/$NETWORKPATH/validator.txt | grep address | cut -f2 -d ":")
 echo $KEYRING | axelard tx snapshot register-proxy $broadcaster --from validator --chain-id $CHAIN_ID --home $HOME/$NETWORKPATH/.core
 echo "done"
 echo
@@ -31,7 +70,7 @@ echo "Leave some balance to pay for fees"
 read -p "Amount of selfstake axltest example: 1000000 (without ${denom}) : " uaxl
 read -p "Enter validator details : " details
 axelarvalconspub=$(echo $KEYRING | axelard tendermint show-validator --home $HOME/$NETWORKPATH/.core)
-echo $KEYRING | axelard tx staking create-validator --yes --amount "${uaxl}${denom}" --moniker "$MONIKER" --commission-rate="0.10" --commission-max-rate="0.20" --commission-max-change-rate="0.01" --min-self-delegation="1" --pubkey=$axelarvalconspub --home $HOME/$NETWORKPATH --chain-id $CHAIN_ID --details "$details" --from validator -b block
+echo $KEYRING | axelard tx staking create-validator --yes --amount "${uaxl}${denom}" --moniker "$MONIKER" --commission-rate="0.10" --commission-max-rate="0.20" --commission-max-change-rate="0.01" --min-self-delegation="1" --pubkey=$axelarvalconspub --home $HOME/$NETWORKPATH --chain-id $CHAIN_ID --details "$details" --home $HOME/$NETWORKPATH/.core --from validator -b block
 echo "done"
 echo
 
